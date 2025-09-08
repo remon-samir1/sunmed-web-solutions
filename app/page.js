ff9498049472be8fa1aa0795f11144d10702a377
remon <remosamir443@gmail.com>
2025-09-08 11:40:49 -0700

    upload

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ import Footer from "./Components/Footer/Footer";
 import Link from "next/link";
 
 export default function Home() {
-  const [size, setSize] = useState((window.innerWidth * 100) / 90);
+  const [size, setSize] = useState(typeof window !== "undefined" && (window.innerWidth * 100) / 90);
   useEffect(() => {
     if (typeof window !== "undefined") {
 
